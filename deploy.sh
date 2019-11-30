aed764d747653f3b27e35fd23e791e05f7480bf1
michael culhane <mike4855@vt.edu>
2019-11-30 14:41:41 -0500

    first commit

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+export app_name=react-test-app
+sudo apt-get update
+sudo apt-get install -y build-essential openssl libssl-dev pkg-config
+sudo apt-get install -y nodejs nodejs-legacy 
+sudo apt-get install npm -y
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+sudo apt-get install nginx git -y
+sudo git clone https://github.com/honoka428/$app_name.git /var/www/$app_name
+sudo apt-get install -y build-essential openssl libssl-dev pkg-config
+sudo cp $HOME/qualcat_deploy/nginx_conf /etc/nginx/sites-available/$app_name
+sudo rm /etc/nginx/sites-available/default
+sudo ln -s /etc/nginx/sites-available/$app_name /etc/nginx/sites-enabled/$app_name
+
+sudo rm /etc/nginx/sites-enabled/default
+sudo npm install pm2 -g
+(cd /var/www/ && sudo chown -R ubuntu $app_name)
+(cd /var/www/$app_name && sudo npm install)
+
+(cd /var/www/$app_name/views && sudo npm install)
+(cd /var/www/$app_name/views && sudo npm run build) # npm ERR! missing script: build
+
+(cd /var/www/$app_name && pm2 start server.js)
+sudo service nginx stop && sudo service nginx start
+
+# Need to run buld and start on both server and client??
